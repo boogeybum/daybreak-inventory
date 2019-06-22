05beb93ba710b08ba4c151b8e12f9be61d0735aa
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-22 11:54:39 -0500

    CRUD screen for item category created

---
+++ models/itemCategoryAdd.js
@@ -0,0 +1,39 @@
+class itemCategoryForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.this.state = {
+      description: '',
+    };
+
+    this.handleChange.bind(this);
+    this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const description = target.description;
+
+    this.setState({ value : event.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit = {this.handleSubmit}>
+        <label>
+          description:
+          <input
+            name = "description"
+            type = "text"
+            value = {this.state.description}
+            onChange = {this.handleInputChange} 
+          />
+        </label>
+        <input 
+          type = "submit" 
+            value = "Submit" 
+          />
+          
+      </form>
+    );
+  }
+}
