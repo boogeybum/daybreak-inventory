900bca464eef2cbcc5adde5d62b13a0805559bdd
Bosco <bosco@MacBook-Air.hsd1.tn.comcast.net>
2019-06-29 19:47:34 -0500

    API to get customers and a table for customer data

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,10 +4,15 @@ export default {
   getitemcategories: () => {
     return axios.get("/api/itemcategory");
   },
-    // This API saves a customer to the database
-    saveCustomer: function(customerData) {
-      return axios.post("/api/customer", customerData);
-    }
+  // This API saves a customer to the database
+  saveCustomer: function (customerData) {
+    return axios.post("/api/customer", customerData);
+  },
+
+  // API to get customers
+  getCustomers: () => {
+    return axios.get("api/customer");
+  }
   // addCategoryToDB: (itemcategoriesData) => {
   //   return axios.post("/api/Itemcategory", itemcategoriesData);
   // }
