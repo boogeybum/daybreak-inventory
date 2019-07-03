025f45b0075d8db0ca039fca215b311c7a95e7d7
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-07-03 15:24:13 -0500

    Removed unused code in table that was commented out

--- client/src/components/lots/lotAdd.js
+++ client/src/components/lots/lotAdd.js
@@ -13,9 +13,6 @@ class LotsForm extends Component {
           purchasePrice: '',
           seller: '',
     };
-  
-      // this.handleChange = this.handleChange.bind(this);
-      // this.handleSubmit = this.handleSubmit.bind(this);
     
     componentDidMount() {
         this.loadLots();
@@ -116,36 +113,7 @@ class LotsForm extends Component {
                     </tbody>
                 </table>
 
-                {/* <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table> */}
+                
             </div>
         );
     }
